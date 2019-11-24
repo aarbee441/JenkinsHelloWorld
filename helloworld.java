664623bdcf995d5a1a86832c524d6a3af10721ce
aarbee441 <57812172+aarbee441@users.noreply.github.com>
2019-11-24 17:20:42 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ public class helloworld
 {
 	public static void main(String[] args)
 		{
-			System.out.println("Integrate with Jenkins");
+			System.out.println("Integrate with Jenkins Its Magic ");
 		}
 }
